4006393d0c5c3cca45607111cca042909cacec23
Ryulynn <78291307+Ryulynn@users.noreply.github.com>
2024-11-23 21:41:55 +0900

    get_quick_tap_termが期待通りに動作しないため、get_tapping_force_holdを使用するように修正

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/custom_v1/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/custom_v1/config.h
@@ -38,4 +38,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
-#define QUICK_TAP_TERM 200
+// #define QUICK_TAP_TERM 200
+#define TAPPING_TERM 200
